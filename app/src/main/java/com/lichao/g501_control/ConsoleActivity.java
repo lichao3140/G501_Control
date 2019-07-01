08acff59e0e98ba85f9e2cd213aeecf507a406b1
lichao3140 <396229938@qq.com>
2019-07-01 14:57:53 +0800

    十六进制转字符串

--- app/src/main/java/com/lichao/g501_control/ConsoleActivity.java
+++ app/src/main/java/com/lichao/g501_control/ConsoleActivity.java
@@ -32,6 +32,9 @@ public class ConsoleActivity extends SerialPortActivity implements View.OnClickL
     EditText mReception, emission;
     Button btn_send;
     String json_str = null;
+    String json_length = null;
+    String temp_str = null;
+    int value_ten;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,21 +73,24 @@ public class ConsoleActivity extends SerialPortActivity implements View.OnClickL
     @Override
     protected void onDataReceived(final byte[] buffer, final int size) {
 
+        String data = new String(buffer, 0, size);
 
+        json_str = json_str + data.replace(" ", "");
+        temp_str = json_str.substring(4, json_str.length());
+        json_length = temp_str.substring(2, 6);
+        value_ten = Integer.parseInt(json_length,16);
 
+        if (temp_str.length() >= value_ten * 2) {
+            //要处理的十六进制数据
+            LogUtil.i("zfr_data", "temp_str:" + temp_str);
+        }
 
         runOnUiThread(new Runnable() {
             public void run() {
                 if (mReception != null) {
                     mReception.append(new String(buffer, 0, size));
-
-                    String data = new String(buffer, 0, size);
-
 //                    String data = "7B22736E223A22373138383032363131222C22757365724944223A223638222C2273746166664944223A223132222C226175746854797065223A2230222C22757365724E616D65223A22E4BDA0222C226175746854696D65223A22323031392D30362D32362031393A33383A3339222C2249444E4F223A22343330343032313938303131323231353358222C2264757479223A22E9A1B9E79BAEE8B49FE8B4A3E4BABA222C2263657274696669636174696F6E223A22222C2249434E4F223A22222C2270686F746F223A22227D";
                     //data = data.substring(data.indexOf("7B22"), data.length());//截取字符串
-
-                    json_str = json_str + data.replace(" ", "");
-                    LogUtil.i("zfr_data", "data:" + data);
 
 //                    data = toStringHex(data);//十六进制转字符串 //{"addInfo":{"result_code":"-1","err_code":"NEED_REVERSAL","err_msg":"exchange cups error"}}
 //                    Log.d("zfr_data1:", data);
